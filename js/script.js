48d564fbedbcc33d764cffdfe1419f7c718643dd
Aayana <annaevanson53@gmail.com>
2025-11-25 18:25:52 -0500

    hamburger fixed

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ hamburger.addEventListener('click', () => {
 dropdown.classList.toggle('hidden');
 });
 
-// Optional: click outside to close
 document.addEventListener('click', (e) => {
 if (!hamburger.contains(e.target) && !dropdown.contains(e.target)) {
     dropdown.classList.add('hidden');
